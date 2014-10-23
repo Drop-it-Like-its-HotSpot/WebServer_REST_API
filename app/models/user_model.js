1fe84afbab829028a8c14e80a216a89647c7a7e8
vatsalvora <vatsal1@msn.com>
2014-10-23 17:34:54 -0400

    Debugging

--- app/models/user_model.js
+++ app/models/user_model.js
@@ -1,8 +1,8 @@
 //Creating a Model for the User Table
-module.export = function(bookshelf)
+module.export = function(bookshelf, Users)
 {
 	return bookshelf.Model.extend({
 		tableName:'Users',
 		idAttribute: 'User_id'
 	});
-};
+}

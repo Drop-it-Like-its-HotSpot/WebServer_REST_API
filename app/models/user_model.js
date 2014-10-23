36da4ce5ba73170bf2665e292e02ae641278c4e8
vatsalvora <vatsal1@msn.com>
2014-10-23 17:38:31 -0400

    Fixing Exports

--- app/models/user_model.js
+++ app/models/user_model.js
@@ -1,8 +1,5 @@
 //Creating a Model for the User Table
-module.export = function(bookshelf, Users)
-{
-	return bookshelf.Model.extend({
+module.export = {
 		tableName:'Users',
 		idAttribute: 'User_id'
-	});
-}
+};

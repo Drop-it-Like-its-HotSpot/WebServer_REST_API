20856ad3051719ebbbf7a53475a665541f90ebf0
Joshua Lee Kegley <josh1289op@gmail.com>
2014-10-24 01:07:22 -0400

    Added route for /messages and /messages/:m_id Not sure if it functions

--- app/routes/messages/messages_route.js
+++ app/routes/messages/messages_route.js
@@ -1,18 +1,16 @@
-//API calls for /api/users to add and get all users
+//API calls for /api/Messages to add and get all messages
 module.exports = function(router, Messages)
 {
-	router.route('/users')
+	router.route('/messages')
 	.post(function(req,res) {
 		var data = ({
-			"m_id":parseInt(req.body.m_id),
-			"Room_id":parseInt(req.body.user_id),
-			"Latitude":Number(req.body.latitude),
-			"Longitude":Number(req.body.longitude),
-			"DisplayName":req.body.displayname,
-			"radius":Number(req.body.radius)
+			"M_id":parseInt(req.body.m_id),
+			"Room_id":parseInt(req.body.room_id),
+			"User_id":parseInt(req.body.user_id),
+			"Message":req.body.message
 		});
 		console.log(data);
-		new Users(data).save({},{method:"insert"}).then(function(result) {
+		new Messages(data).save({},{method:"insert"}).then(function(result) {
 				res.send(result.toJSON());
 			}).catch(function(error) {
 				  console.log(error);
@@ -20,7 +18,7 @@ module.exports = function(router, Messages)
 			});
 	})
 	.get(function(req,res){
-		new Users().fetchAll()
+		new Messages().fetchAll()
 		.then(function(result) {
 		  res.send(result.toJSON());
 		}).catch(function(error) {
@@ -29,3 +27,38 @@ module.exports = function(router, Messages)
 		});
 	});
 };
+
+//API Call for /api/messages/:m_id to get, update, and delete a specific user
+router.route('/messages/:m_id')
+.get(function(req,res){
+	new Messages({"m_id":parseInt(req.params.m_id)}).fetch()
+    .then(function(result) {
+      res.send(result.toJSON());
+    }).catch(function(error) {
+      console.log(error);
+      res.send('An error occured');
+    });
+})
+.delete(function(req,res){
+	new Messages({"m_id":parseInt(req.params.m_id)}).destroy()
+    .then(function(result) {
+      res.send(result.toJSON());
+    }).catch(function(error) {
+      console.log(error);
+      res.send('An error occured');
+    });
+})
+.put(function(req,res){
+	var data = ({});
+	if(req.body.room_id !== undefined) data.Room_id = Number(req.body.room_id);
+	if(req.body.user_id !== undefined) data.User_id = Number(req.body.user_id);
+	if(req.body.message !== undefined) data.Message = req.body.message;
+	console.log(data);
+	new Messages({"m_id":parseInt(req.params.m_id)}).save(data,{patch:true})
+    .then(function(result) {
+      res.send(result.toJSON());
+    }).catch(function(error) {
+      console.log(error);
+      res.send('An error occured');
+    });
+});

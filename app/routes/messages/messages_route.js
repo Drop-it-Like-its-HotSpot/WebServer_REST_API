dcceb3a56200e0a5f137e1eacdfc552f80f7c34a
Joshua Lee Kegley <josh1289op@gmail.com>
2014-10-24 00:43:10 -0400

    adding Messaging support

---
+++ app/routes/messages/messages_route.js
@@ -0,0 +1,31 @@
+//API calls for /api/users to add and get all users
+module.exports = function(router, Messages)
+{
+	router.route('/users')
+	.post(function(req,res) {
+		var data = ({
+			"User_id":parseInt(req.body.user_id),
+			"Email_id":req.body.email_id,
+			"Latitude":Number(req.body.latitude),
+			"Longitude":Number(req.body.longitude),
+			"DisplayName":req.body.displayname,
+			"radius":Number(req.body.radius)
+		});
+		console.log(data);
+		new Users(data).save({},{method:"insert"}).then(function(result) {
+				res.send(result.toJSON());
+			}).catch(function(error) {
+				  console.log(error);
+				  res.send('An error occured');
+			});
+	})
+	.get(function(req,res){
+		new Users().fetchAll()
+		.then(function(result) {
+		  res.send(result.toJSON());
+		}).catch(function(error) {
+		  console.log(error);
+		  res.send('An error occured');
+		});
+	});
+};

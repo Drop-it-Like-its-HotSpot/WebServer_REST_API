97386abe6738f3cf4dc02659097301e5f476c999
vatsalvora <vatsal1@msn.com>
2014-10-24 01:10:57 -0400

    Fixed Restart.sh code

--- restart.sh
+++ restart.sh
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-sudo su
-cd
-cd WebServer
-forever stopall
-forever start server.js
-=======
 #!/bin/bash
 cd
 cd WebServer
 forever stopall
 forever start server.js
 echo "Server Restarted"
->>>>>>> 4e65f3c68ee84d1b5f1a9c7efb516007ce96f7c3

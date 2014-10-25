8f49fc42262de68a034741a5efa1bbf3c7db7842
Joshua Lee Kegley <josh1289op@gmail.com>
2014-10-25 14:42:13 -0400

    adding messages/room_id/:room_id

--- app/routes/messages/messages_route.js
+++ app/routes/messages/messages_route.js
@@ -62,5 +62,40 @@ module.exports = function(router, Messages)
 		  res.send('An error occured');
 		});
 	});
+	
+	//API Call for /api/messages/room_id/:room_id to get, update, and delete a specific 
+	router.route('/messages/room_id/:room_id')
+	.get(function(req,res){
+		new Messages({"Room_id'":parseInt(req.params.room_id')}).fetch()
+		.then(function(result) {
+		  res.send(result.toJSON());
+		}).catch(function(error) {
+		  console.log(error);
+		  res.send('An error occured');
+		});
+	})
+	.delete(function(req,res){
+		new Messages({"Room_id'":parseInt(req.params.room_id')}).destroy()
+		.then(function(result) {
+		  res.send(result.toJSON());
+		}).catch(function(error) {
+		  console.log(error);
+		  res.send('An error occured');
+		});
+	})
+	.put(function(req,res){
+		var data = ({});
+		if(req.body.m_id !== undefined) data.m_id = Number(req.body.m_id);
+		if(req.body.user_id !== undefined) data.User_id = Number(req.body.user_id);
+		if(req.body.message !== undefined) data.Message = req.body.message;
+		console.log(data);
+		new Messages({"Room_id'":parseInt(req.params.room_id')}).save(data,{patch:true})
+		.then(function(result) {
+		  res.send(result.toJSON());
+		}).catch(function(error) {
+		  console.log(error);
+		  res.send('An error occured');
+		});
+	});
 
 };

8d1330e33107404b88bc525dd30b8420de0ac2d5
vatsalvora <vatsal1@msn.com>
2014-10-27 17:14:52 -0400

    debugging

--- app/routes/login/login_route.js
+++ app/routes/login/login_route.js
@@ -14,8 +14,8 @@ module.exports = function(router, Users, Cred, Session)
                 var sessionid = uuid.v1();
                 new Cred({"User_id":uid}).fetch({require:true}).then(function(model) {
                     var password = model.get("Password");
-                    bcrypt.compare(req.body.password, password, function(error, res) {
-                       if(res){
+                    bcrypt.compare(req.body.password, password, function(error, response) {
+                       if(response){
                            new Session().save({"User_id":uid,"session_id":sessionid},{method:"insert"}).then(function(result) {
                                var message =  result.toJSON();
                                message.success = true;

379a497df2d689a05f30fed9a3f9b83e5cdc3b6b
Ted Turner <tedturner29@ufl.edu>
2014-10-26 20:15:15 -0400

    Added ChatRoom routes

---
+++ app/routes/chatroom/chatroom_chatroomid_route.js
@@ -0,0 +1,40 @@
+module.exports = function(router)
+{
+	//API Call for /api/chatroom/:chat_id to get, update, and delete a specific user
+	router.route('/chatroom/:chat_id')
+	.get(function(req,res){
+		new ChatRoom({"chat_id":parseInt(req.params.chat_id)}).fetch()
+		.then(function(result) {
+		  res.send(result.toJSON());
+		}).catch(function(error) {
+		  console.log(error);
+		  res.send('An error occured');
+		});
+	})
+	.delete(function(req,res){
+		new ChatRoom({"chat_id":parseInt(req.params.chat_id)}).destroy()
+		.then(function(result) {
+		  res.send(result.toJSON());
+		}).catch(function(error) {
+		  console.log(error);
+		  res.send('An error occured');
+		});
+	})
+	.put(function(req,res){
+		var data = ({});
+		if(req.body.room_admin !== undefined) data.Room_Admin = parseInt(req.body.room_admin);
+		if(req.body.latitude !== undefined) data.Latitude = Number(req.body.latitude);
+		if(req.body.longitude !== undefined) data.Longitude = Number(req.body.longitude);
+		if(req.body.chat_title !== undefined) data.Chat_title = req.body.chat_title.trim();
+		if(req.body.chat_dscrpn !== undefined) data.Chat_Dscrpn = req.body.chat_dscrpn.trim();
+		
+		console.log(data);
+		new Chat_Room({"chat_id":parseInt(req.params.chat_id)}).save(data,{patch:true})
+		.then(function(result) {
+		  res.send(result.toJSON());
+		}).catch(function(error) {
+		  console.log(error);
+		  res.send('An error occured');
+		});
+	});
+}
---
+++ app/routes/chatroom/chatroom_route.js
@@ -0,0 +1,32 @@
+module.exports = function(router)
+{
+	//API calls for /api/chatroom to add and get all chatrooms
+	router.route('/chatroom')
+	.post(function(req,res) {
+		var data = ({
+			"chat_id":parseInt(req.body.chat_id),
+			"Room_Admin":parseInt(req.body.room_admin),
+			"Latitude":Number(req.body.latitude),
+			"Longitude":Number(req.body.longitude),
+			"Chat_title":req.body.chat_title,
+			"Chat_Dscrpn":req.body.chat_dscrpn
+		});
+		console.log(data);
+		new ChatRoom(data).save({},{method:"insert"}).then(function(result) {
+				res.send(result.toJSON());
+			}).catch(function(error) {
+				  console.log(error);
+				  res.send('An error occured');
+			});
+	})
+
+	.get(function(req,res){
+		new ChatRoom().fetchAll()
+		.then(function(result) {
+		  res.send(result.toJSON());
+		}).catch(function(error) {
+		  console.log(error);
+		  res.send('An error occured');
+		});
+	});
+}

26086269316ca9931ab69f91f3ef624da3f7aaca
Ted Turner <tedturner29@ufl.edu>
2014-10-27 10:59:41 -0400

    fixed email id reference, Attempt 2

--- app/routes/user/user_route.js
+++ app/routes/user/user_route.js
@@ -12,7 +12,7 @@ module.exports = function(router, Users, Cred)
 			"radius":Number(req.body.radius)
 		});
 		console.log(data);
-		new Users({data["Email_id"]}).fetch()
+		new Users({req.body.email_id}).fetch()
 		.then(function(result) {
 		  if(typeof result !== 'undefined'){
 			throw 'Email already exists';

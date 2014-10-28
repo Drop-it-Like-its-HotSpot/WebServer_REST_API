b4417ab1ce850da65d3d1d4749f0e502438960df
Ted Turner <tedturner29@ufl.edu>
2014-10-28 12:52:08 -0400

    fix for password comparison in login

--- app/routes/login/login_route.js
+++ app/routes/login/login_route.js
@@ -14,9 +14,10 @@ module.exports = function(router, Users, Cred, Session)
                 console.log(uid);
                 var sessionid = uuid.v1();
                 new Cred({"User_id":uid}).fetch({require:true}).then(function(model) {
-                    var password = model.get("Password");
+                    var password = model.get("Password").trim();
                     console.log(password);
                     console.log(req.body.password.trim());
+					
                     bcrypt.compare(req.body.password.trim(), password, function(error, response) {
                        if(response === true){
                            new Session().save({"User_id":uid,"session_id":sessionid},{method:"insert"}).then(function(result) {

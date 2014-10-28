85a86bae54924385ca6be7b8d7a5b8f4f15d1c3b
Ted Turner <tedturner29@ufl.edu>
2014-10-28 16:57:22 -0400

    fixed bug in check session

--- app/routes/session/check_session.js
+++ app/routes/session/check_session.js
@@ -3,7 +3,7 @@ module.exports = function(session_id,Session)
 {
 	var moment = require('moment');
 	console.log("YAY it Works");
-	new Session({"session_id":session_id)}).fetch({require:true}).then(function(model) {
+	new Session({"session_id":session_id}).fetch({require:true}).then(function(model) {
 		var timestamp = moment(model.get("timestamp"));
 		var diff = moment().diff(timestamp, 'seconds');
 		console.log(timestamp);

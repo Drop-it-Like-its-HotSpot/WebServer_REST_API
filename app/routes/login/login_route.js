665ac0e119650f361e671a1d3eba0b71832b24ee
Ted Turner <tedturner29@ufl.edu>
2014-10-28 16:28:09 -0400

    Check session added

--- app/routes/login/login_route.js
+++ app/routes/login/login_route.js
@@ -3,41 +3,42 @@ module.exports = function(router, Users, Cred, Session)
 {
     var bcrypt = require('bcrypt');
     var uuid = require('node-uuid');
+	
     router.route('/login')
-        .post(function(req,res) {
-            var data = ({
-                "Email_id":req.body.email_id,
-            });
-            console.log(data);
-            new Users(data).fetch({require:true}).then(function(model) {
-                var uid = model.get("User_id");
-                console.log(uid);
-                var sessionid = uuid.v4();
-                new Cred({"User_id":uid}).fetch({require:true}).then(function(model) {
-                    var password = model.get("Password").trim();
-                    bcrypt.compare(req.body.password.trim(), password, function(error, response) {
-                       if(response === true){
-                           new Session().save({"User_id":uid,"session_id":sessionid},{method:"insert"}).then(function(result) {
-                               var message =  result.toJSON();
-                               message.success = true;
-                               res.send(message);
-                           }).catch(function(error) {
-                               console.log(error);
-                               res.send('An error occured');
-                           });
-                       }
-                        else{
-                           console.log(response);
-                           res.send('An error occured');
-                       }
-                    });
-                }).catch(function(error) {
-                    console.log(error);
-                    res.send('An error occured');
-                });
-            }).catch(function(error) {
-                console.log(error);
-                res.send('An error occured');
-            });
-        });
+	.post(function(req,res) {
+		var data = ({
+			"Email_id":req.body.email_id,
+		});
+		console.log(data);
+		new Users(data).fetch({require:true}).then(function(model) {
+			var uid = model.get("User_id");
+			console.log(uid);
+			var sessionid = uuid.v4();
+			new Cred({"User_id":uid}).fetch({require:true}).then(function(model) {
+				var password = model.get("Password").trim();
+				bcrypt.compare(req.body.password.trim(), password, function(error, response) {
+				   if(response === true){
+					   new Session().save({"User_id":uid,"session_id":sessionid},{method:"insert"}).then(function(result) {
+						   var message =  result.toJSON();
+						   message.success = true;
+						   res.send(message);
+					   }).catch(function(error) {
+						   console.log(error);
+						   res.send('An error occured');
+					   });
+				   }
+					else{
+					   console.log(response);
+					   res.send('An error occured');
+				   }
+				});
+			}).catch(function(error) {
+				console.log(error);
+				res.send('An error occured');
+			});
+		}).catch(function(error) {
+			console.log(error);
+			res.send('An error occured');
+		});
+	});
 };
---
+++ app/routes/session/check_session.js
@@ -0,0 +1,17 @@
+//Session File
+module.exports = function(req,Session) 
+{
+	var moment = require('moment');
+
+	new Session({"session_id":req.body.session_id, "User_id":parseInt(req.params.user_id)}).fetch({require:true}).then(function(model) {
+		var timestamp = moment(model.get("timestamp"));
+		var diff = moment().diff(timestamp, 'seconds');
+		if (diff > 60) {
+			throw "Session expired";
+		}
+		return true;
+	}).catch(function(error) {
+		console.log(error);
+		res.send('An error occured');
+	});
+} 

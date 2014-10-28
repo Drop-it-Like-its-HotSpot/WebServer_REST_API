7bc78e9580c81a681f48e1e35b0ab10e7e04f05e
Ted Turner <tedturner29@ufl.edu>
2014-10-28 16:43:32 -0400

    adding debug information to check session

--- app/routes/session/check_session.js
+++ app/routes/session/check_session.js
@@ -6,6 +6,9 @@ module.exports = function(req,Session)
 	new Session({"session_id":req.body.session_id, "User_id":parseInt(req.params.user_id)}).fetch({require:true}).then(function(model) {
 		var timestamp = moment(model.get("timestamp"));
 		var diff = moment().diff(timestamp, 'seconds');
+		console.log(timestamp);
+		console.log(diff);
+		
 		if (diff > 60) {
 			throw "Session expired";
 		}

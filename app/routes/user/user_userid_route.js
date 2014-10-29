8ce9e07ae045dc9273708184d95aabe6252aba37
Ted Turner <tedturner29@ufl.edu>
2014-10-29 16:24:13 -0400

    Added check session to all user functions

--- app/routes/user/user_userid_route.js
+++ app/routes/user/user_userid_route.js
@@ -5,9 +5,23 @@ module.exports = function(router, Users, Session)
 	
 	router.route('/users/:user_id/:session_id')
 	.get(function(req,res){
-		new Users({"User_id":parseInt(req.params.user_id)}).fetch()
-		.then(function(result) {
-		  res.send(result.toJSON());
+		new Session({"session_id":req.params.session_id}).fetch({require:true}).then(function(model) {
+			console.log("Session found");
+			var result = check_session(Session,req.params.session_id,model.get('timestamp'))
+			console.log("Result: " + result);
+			if (result === true) {
+				new Users({"User_id":parseInt(req.params.user_id)}).fetch()
+				.then(function(result) {
+					res.send(result.toJSON());
+				}).catch(function(error) {
+					console.log(error);
+					res.send('An error occured');
+				});
+			}
+			else {
+				console.log("Session Expired");
+				res.send('Session Expired');
+			}
 		}).catch(function(error) {
 		  console.log(error);
 		  res.send('An error occured');
@@ -39,21 +53,29 @@ module.exports = function(router, Users, Session)
 	})
 	.put(function(req,res){
 		new Session({"session_id":req.body.session_id}).fetch({require:true}).then(function(model) {
-			var data = ({});
-			if(req.body.email_id !== undefined) data.Email_id = req.body.email_id.trim();
-			if(req.body.latitude !== undefined) data.Latitude = Number(req.body.latitude);
-			if(req.body.longitude !== undefined) data.Longitude = Number(req.body.longitude);
-			if(req.body.displayname !== undefined) data.DisplayName = req.body.displayname.trim();
-			if(req.body.radius !== undefined) data.radius = Number(req.body.radius);
-			
-			console.log(data);
-			new Users({"User_id":parseInt(req.params.user_id)}).save(data,{patch:true})
-			.then(function(result) {
-			  res.send(result.toJSON());
-			}).catch(function(error) {
-			  console.log(error);
-			  res.send('An error occured');
-			});
+			var result = check_session(Session,req.body.session_id,model.get('timestamp'))
+			console.log("Result: " + result);
+			if (result === true) {
+				var data = ({});
+				if(req.body.email_id !== undefined) data.Email_id = req.body.email_id.trim();
+				if(req.body.latitude !== undefined) data.Latitude = Number(req.body.latitude);
+				if(req.body.longitude !== undefined) data.Longitude = Number(req.body.longitude);
+				if(req.body.displayname !== undefined) data.DisplayName = req.body.displayname.trim();
+				if(req.body.radius !== undefined) data.radius = Number(req.body.radius);
+				
+				console.log(data);
+				new Users({"User_id":parseInt(req.params.user_id)}).save(data,{patch:true})
+				.then(function(result) {
+				  res.send(result.toJSON());
+				}).catch(function(error) {
+				  console.log(error);
+				  res.send('An error occured');
+				});
+			}
+			else {
+				console.log("Session Expired");
+				res.send('Session Expired');
+			}
 		}).catch(function(error) {
 			  console.log(error);
 			  res.send('An error occured');

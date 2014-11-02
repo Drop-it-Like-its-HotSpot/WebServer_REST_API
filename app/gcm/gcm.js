45a30e18f5f7b05b42702c2aed3e8cbcb7820c02
vatsalvora <vatsal1@msn.com>
2014-11-02 16:33:50 -0500

    Added gcm functionality

---
+++ app/gcm/gcm.js
@@ -0,0 +1,14 @@
+//Google Cloud Messaging Function
+module.exports = function(data,r_ids) 
+{
+	var gcm = require('node-gcm');
+
+	// create a message with default values
+	var message = new gcm.Message();
+	var apiKey = require('./config/gcmconfig.js');
+	var sender = new gcm.Sender(apiKey);
+	message.addDataWithObject(data);
+	sender.send(message, r_ids, 4, function (err, result) {
+		console.log(result);
+	});
+}

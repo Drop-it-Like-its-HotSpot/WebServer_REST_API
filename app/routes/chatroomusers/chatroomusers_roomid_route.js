1b90ace144303a96a67d98da3e1e9e666a06525a
Ted Turner <tedturner29@ufl.edu>
2014-11-07 10:24:33 -0500

    changed fetch to fetchAll

--- app/routes/chatroomusers/chatroomusers_roomid_route.js
+++ app/routes/chatroomusers/chatroomusers_roomid_route.js
@@ -9,7 +9,7 @@ module.exports = function(router, ChatRoomUsers, Session)
 			var result = check_session(Session,req.params.session_id,model.get('timestamp'))
 			console.log("Result: " + result);
 			if (result === true) {
-				new ChatRoomUsers({"Room_id":req.params.room_id}).fetch()
+				new ChatRoomUsers({"Room_id":req.params.room_id}).fetchAll()
 				.then(function(result) {
 					res.send(result.toJSON());
 				}).catch(function(error) {

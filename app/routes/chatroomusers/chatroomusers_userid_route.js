a214bd30e2e64b21585d46aa7ba1897f30977a76
vatsalvora <vatsal1@msn.com>
2014-11-08 15:23:02 -0500

    Using user_id from session model

--- app/routes/chatroomusers/chatroomusers_userid_route.js
+++ app/routes/chatroomusers/chatroomusers_userid_route.js
@@ -2,14 +2,15 @@ module.exports = function(router, ChatRoomUsers, Session)
 {
 	//API calls for /api/chatroomusers to add and get all chatroomusers
 	var check_session = require('../session/check_session');
-	router.route('/chatroomusers/user_id/:user_id/:session_id')
+	router.route('/chatroomusers/user_id/:session_id')
 	.get(function(req,res){
 		new Session({"session_id":req.params.session_id}).fetch({require:true}).then(function(model) {
 			console.log("Session found");
 			var result = check_session(Session,req.params.session_id,model.get('timestamp'))
 			console.log("Result: " + result);
+			var uid = model.get("User_id");
 			if (result === true) {
-				new ChatRoomUsers().where({"User_id":parseInt(req.params.user_id)}).fetchAll()
+				new ChatRoomUsers().where({"User_id":parseInt(uid)}).fetchAll()
 				.then(function(result) {
 					res.send(result.toJSON());
 				}).catch(function(error) {

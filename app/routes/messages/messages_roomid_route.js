1a2cdf1e1290f62ce8c48e38f61f5a0eb9bd4d1a
vatsalvora <vatsal1@msn.com>
2014-11-09 22:09:13 -0500

    Getting only messages since joining chatroom

--- app/routes/messages/messages_roomid_route.js
+++ app/routes/messages/messages_roomid_route.js
@@ -1,5 +1,5 @@
 //API calls for /api/messages/room_id to add and get all messages
-module.exports = function(router, Messages, Session)
+module.exports = function(router, Messages, ChatRoomUsers, Session)
 {
 	var check_session = require('../session/check_session');
 	
@@ -9,29 +9,40 @@ module.exports = function(router, Messages, Session)
 		new Session({"session_id":req.params.session_id}).fetch({require:true}).then(function(model) {
 			console.log("Session found");
 			var result = check_session(Session,req.params.session_id,model.get('timestamp'))
+			var uid = model.get("User_id");
 			console.log("Result: " + result);
-			if (result === true) {
-				new Messages().query(function(qb)
-					{
-						qb.where("Room_id","=",parseInt(req.params.room_id))
-						.orderBy("TimeStamp","asc");
-					}
-				)
-				.fetchAll()
-				.then(function(result) {
-				  res.send(result.toJSON());
-				}).catch(function(error) {
-				  console.log(error);
-				  res.send('An error occured');
-				});
+			new ChatRoomUsers({"Room_id":req.params.room_id,"User_id":uid}).fetch({require:true})
+			.then(function(crumodel){
+				if (result === true) {
+					new Messages().query(function(qb)
+						{
+							qb.where("Room_id","=",parseInt(req.params.room_id))
+							.andWhere("TimeStamp",">",crumodel.get("joined"))
+							.orderBy("TimeStamp","asc");
+						}
+					)
+					.fetchAll()
+					.then(function(result) {
+					  var ret = result.toJSON();
+					  ret.success = true;
+					  res.send(ret);
+					}).catch(function(error) {
+					  console.log(error);
+					  res.json({success:false});
+					});
+				}
+				else {
+					console.log("Session Expired");
+					res.json({success:false});
+				}
 			}
-			else {
-				console.log("Session Expired");
-				res.send('Session Expired');
-			}
+			}).catch(function(error) {
+			  console.log(error);
+			  res.json({success:false});
+			});
 		}).catch(function(error) {
 		  console.log(error);
-		  res.send('An error occured');
+		  res.json({success:false});
 		});
 	});
 	router.route('/messages/room_id/:room_id/:timestamp/:session_id')

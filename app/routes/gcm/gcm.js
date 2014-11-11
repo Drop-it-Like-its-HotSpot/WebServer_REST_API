cd3d9641b2bff0ba26ebb9c9a7991de44ba27e23
vatsalvora <vatsal1@msn.com>
2014-11-11 00:42:29 -0500

    Debugging

--- app/routes/gcm/gcm.js
+++ app/routes/gcm/gcm.js
@@ -16,15 +16,18 @@ module.exports = function(data,u_ids, GCMDB, knex)
 	}
 	raw += "'User_id' = ?";
 	
-	r_ids = [];
+	r_ids = [93];
 	console.log("Came here!!");
 	console.log(raw);
 
-	knex('gcm').whereRaw(raw,u_ids).then(function(result) {
+	//knex('gcm').whereRaw(raw,u_ids)
+	new GCMDB().where({"User_id":93}).fetch({require:true})
+	.then(function(result) {
 		console.log(result);
 	}).catch(function(error) {
 		console.log(error);
 	});
+	
 	
 	sender.send(message, r_ids, 4, function (err, result) {
 		console.log(result);

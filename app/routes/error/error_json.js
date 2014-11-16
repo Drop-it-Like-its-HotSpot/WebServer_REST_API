cd94c2fd161d49c82ff91573f8082d102f9f6ca0
Ted Turner <tedturner29@ufl.edu>
2014-11-16 17:59:35 -0500

    will fix error method later

--- app/routes/error/error_json.js
+++ app/routes/error/error_json.js
@@ -2,6 +2,6 @@
 module.exports = function(ErrorCode) 
 {
 	var fs = require('fs');
-	var obj = JSON.parse(fs.readFileSync('../error/ErrorList.json', 'utf8'));
-	return {error_code:ErrorCode,success:false,info:obj.get(ErrorCode)};
+	//var obj = JSON.parse(fs.readFileSync('../error/ErrorList.json', 'utf8'));
+	return {error_code:ErrorCode,success:false};//,info:obj.get(ErrorCode)};
 } 

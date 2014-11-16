c0467d3e235b3e97757bf1cc98dfe13ffe6057d7
vatsalvora <vatsal1@msn.com>
2014-11-16 18:38:03 -0500

    added route for testing GCM

--- app/routes/gcm/gcm_send_route.js
+++ app/routes/gcm/gcm_send_route.js
@@ -1,7 +1,7 @@
 //API calls for /api/gcm to save registration ids
 module.exports = function(router, GCMDB, knex)
 {
-	var gcm = require('gcm');
+	var gcm = require('../gcm');
 	router.route('/gcm/send')
 	.post(function(req,res) {
 		var result = gcm(req.body.data,[req.body.uid],GCMDB, knex);

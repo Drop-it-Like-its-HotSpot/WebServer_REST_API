f9284a472ddd75d8b8981bb98f1a0a825b99e8ee
vatsalvora <vatsal1@msn.com>
2014-11-16 22:25:51 -0500

    Debugging

--- app/routes/gcm/gcm_send_route.js
+++ app/routes/gcm/gcm_send_route.js
@@ -4,7 +4,8 @@ module.exports = function(router, GCMDB, knex)
 	var gcm = require('../gcm/gcm');
 	router.route('/gcm/send')
 	.post(function(req,res) {
-		var data = {Test:req.body.data}
+		var data = {Test:req.body.data};
+		console.log(JSON.parse(req.body.uids));
 		gcm(data,JSON.parse(req.body.uids),GCMDB, knex, res);
 	});
 }

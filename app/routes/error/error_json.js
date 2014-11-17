cb108259f52c8d0b84ac92e0b5106b3fded8fd1f
Ted Turner <tedturner29@ufl.edu>
2014-11-16 21:17:56 -0500

    working on error_json 5

--- app/routes/error/error_json.js
+++ app/routes/error/error_json.js
@@ -3,11 +3,11 @@ module.exports = function(ErrorCode)
 {
 	var fs = require('fs');
 	console.log(fs.readdirSync('.'));
-	fs.readFile('./app/route/error/ErrorList.json', 'utf8', function(err, data) {
+	fs.readFile('./app/routes/error/ErrorList.json', 'utf8', function(err, data) {
 		if (err) throw err;
 		console.log(data);
 		var obj = JSON.parse(data);
 		console.log(obj);
-		return {error_code:ErrorCode,success:false};//,info:obj.get(ErrorCode)};
+		return {error_code:ErrorCode,success:false,info:obj.get(ErrorCode)};
 	});
 } 

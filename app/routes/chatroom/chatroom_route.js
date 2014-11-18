14f626d2bf72ad6900355109d2b68c5123344638
Ted Turner <tedturner29@ufl.edu>
2014-11-18 15:55:51 -0500

    fixed the bug

--- app/routes/chatroom/chatroom_route.js
+++ app/routes/chatroom/chatroom_route.js
@@ -86,12 +86,12 @@ module.exports = function(router, ChatRoom, Session, Users, ChatRoomUsers, knex)
 			
 			if (result === true) {
 				new Users({"User_id":model.get("User_id")}).fetch({require:true}).then(function(userModel) {
-					var raw = '(acos(sin(radians('+userModel.get("chat_room.Latitude")+'))*sin(radians("chat_room.Latitude")) + cos(radians('+userModel.get("chat_room.Latitude")+'))*cos(radians("chat_room.Latitude"))*cos(radians("chat_room.Longitude")-radians('+userModel.get("Longitude")+'))) * 6371 < ('+userModel.get("radius")+' * 1.6))';
+					var raw = '(acos(sin(radians('+userModel.get("Latitude")+'))*sin(radians("chat_room.Latitude")) + cos(radians('+userModel.get("Latitude")+'))*cos(radians("chat_room.Latitude"))*cos(radians("chat_room.Longitude")-radians('+userModel.get("Longitude")+'))) * 6371 < ('+userModel.get("radius")+' * 1.6))';
 					knex('chat_room')
 					.whereRaw(raw)
 					.join('users','chat_room.Room_Admin', '=', 'users.User_id')
-					.select('chat_room.chat_id', 'users.DisplayName')
-					.orderBy('chat_room.chat_id', 'desc')
+					.select('chat_room.*', 'users.DisplayName')
+					.orderBy('chat_id', 'desc')
 					.then(function(result) {
 						console.log('query success');
 						var ChatRoomList = result;

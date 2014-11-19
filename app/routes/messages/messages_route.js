fc991b66c70926e7e36e22b511347af4c479b3ec
Ted Turner <tedturner29@ufl.edu>
2014-11-19 14:58:06 -0500

    changed post message return 2

--- app/routes/messages/messages_route.js
+++ app/routes/messages/messages_route.js
@@ -29,6 +29,7 @@ module.exports = function(router, Messages, Session, GCMDB, io, knex, ChatRoomUs
 				});
 				
 				new Messages().save(data,{method:"insert"}).then(function(message_result) {
+					var ret = message_result.toJSON();
 					new ChatRoomUsers().where({"Room_id":parseInt(req.body.room_id)}).fetchAll()
 					.then(function(result) {
 						var user_arr = result;
@@ -43,7 +44,8 @@ module.exports = function(router, Messages, Session, GCMDB, io, knex, ChatRoomUs
 						console.log(u_ids);
 						gcm(data,u_ids,GCMDB, knex);
 						io.to(req.body.room_id).emit("New Message!");
-						res.send(message_result.toJSON());
+						ret.success = true;
+						res.send(ret);
 					}).catch(function(error) {
 						console.log(error);
 						res.send(error_json("141"));

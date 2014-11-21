bd3cbfb6d88c4ba954c40b97a92bade21f9439e6
vatsalvora <vatsal1@msn.com>
2014-11-21 10:26:53 -0500

    testing

--- app/routes/gcm/gcm.js
+++ app/routes/gcm/gcm.js
@@ -10,18 +10,15 @@ module.exports = function(data,u_ids, GCMDB, knex)
 	message.addDataWithObject(data);
 	
 	console.log("Came here!!");
-	console.log(u_ids);
 	r_ids = [];
 	var response = [];
 
 	knex('gcm')
 	.whereIn("User_id",u_ids)
 	.then(function(result) {
-		console.log(result);
 		for( u in result)
 		{
 			r_ids.push(result[u]["reg_id"]);
-			console.log(r_ids);
 			sender.send(message, r_ids, 4, function (err, ret) {
 
 				if(err !== null) console.log(err);

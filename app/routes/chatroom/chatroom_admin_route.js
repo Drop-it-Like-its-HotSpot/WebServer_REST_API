3234c949437734db7fa14f83687e5a6c71be9db2
Ted Turner <tedturner29@ufl.edu>
2014-11-30 10:49:56 -0500

    added request to get all chatrooms user is admin of

---
+++ app/routes/chatroom/chatroom_admin_route.js
@@ -0,0 +1,27 @@
+//API calls for /api/chatroom to add and get all chatrooms
+module.exports = function(router, ChatRoom, Session, Users, ChatRoomUsers, knex, error_json, success_json, check_session)
+{
+	router.route('/chatroom/admin/:session_id')
+	.get(function(req,res){
+		new Session({"session_id":req.params.session_id}).fetch({require:true}).then(function(model) {
+			var result = check_session(Session,req.params.session_id,model.get('timestamp'));
+			var uid = model.get("User_id");
+			
+			if (result === true) {
+				new ChatRoom({"Room_Admin":uid}).fetchAll().then(function(chatroomModel) {
+					res.send(success_json(chatroomModel.toJSON()));
+				}).catch(function(error) {
+					console.log(error);
+					res.send(error_json("131"));
+				});
+			}
+			else {
+				console.log("Session Expired");
+				res.json(error_json("103"));
+			}
+		}).catch(function(error) {
+			console.log(error);
+			res.send(error_json("101"));
+		});
+	});
+};

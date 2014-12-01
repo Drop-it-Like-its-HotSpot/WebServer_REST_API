13b9d8fea8a7e53a0c266cf8b6ba69406505d892
vatsalvora <vatsal1@msn.com>
2014-11-30 20:43:15 -0500

    Using session to get UID

--- app/routes/user/user_userid_route.js
+++ app/routes/user/user_userid_route.js
@@ -1,29 +1,7 @@
 //API Call for /api/users/:user_id to get, update, and delete a specific user
 module.exports = function(router, Users, Session, error_json, success_json, check_session)
 {
-	router.route('/users/:user_id/:session_id')
-	.get(function(req,res){
-		new Session({"session_id":req.params.session_id}).fetch({require:true}).then(function(model) {
-			var result = check_session(Session,req.params.session_id,model.get('timestamp'))
-			if (result === true) {
-				new Users({"User_id":parseInt(req.params.user_id)}).fetch()
-				.then(function(result) {
-					res.send(success_json(result.toJSON()));
-				}).catch(function(error) {
-					console.log(error);
-					res.send(error_json("111"));
-				});
-			}
-			else {
-				console.log("Session Expired");
-				res.send(error_json("103"));
-			}
-		}).catch(function(error) {
-			console.log(error);
-			res.send(error_json("101"));
-		});
-	});
-	router.route('/users/delete/:user_id')
+	router.route('/users/delete/')
 	.post(function(req,res){
 		if(req.body.session_id === undefined) {
 			res.json({missing_parameter:"session_id",success:false});
@@ -31,8 +9,9 @@ module.exports = function(router, Users, Session, error_json, success_json, chec
 		}
 		new Session({"session_id":req.body.session_id}).fetch({require:true}).then(function(model) {
 			var result = check_session(Session,req.body.session_id,model.get('timestamp'))
+			var uid = model.get("User_id");
 			if (result === true) {
-				new Users({"User_id":parseInt(req.params.user_id)}).destroy()
+				new Users({"User_id":parseInt(uid)}).destroy()
 				.then(function(result) {
 					res.send(success_json(result.toJSON()));
 				}).catch(function(error) {
@@ -50,10 +29,11 @@ module.exports = function(router, Users, Session, error_json, success_json, chec
 			res.send(error_json("101"));
 		});
 	})
-	router.route('/users/put/:user_id')
+	router.route('/users/put/')
 	.post(function(req,res){
 		new Session({"session_id":req.body.session_id}).fetch({require:true}).then(function(model) {
 			var result = check_session(Session,req.body.session_id,model.get('timestamp'))
+			var uid = model.get("User_id");
 			if (result === true) {
 				var data = ({});
 				if(req.body.email_id !== undefined) data.Email_id = req.body.email_id.trim();
@@ -62,7 +42,7 @@ module.exports = function(router, Users, Session, error_json, success_json, chec
 				if(req.body.displayname !== undefined) data.DisplayName = req.body.displayname.trim();
 				if(req.body.radius !== undefined) data.radius = Number(req.body.radius);
 				
-				new Users({"User_id":parseInt(req.params.user_id)}).save(data,{patch:true})
+				new Users({"User_id":parseInt(uid)}).save(data,{patch:true})
 				.then(function(result) {
 					res.send(success_json(result.toJSON()));
 				}).catch(function(error) {
